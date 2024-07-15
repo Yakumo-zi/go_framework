c025d427a43610b96336b3470e317740a7284d8d
Yakumo-zi <1477717283@qq.com>
2024-07-15 13:48:43 +0000

    feat:add service register support

--- rpc/cmd/main.go
+++ rpc/cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"rpc"
@@ -9,7 +8,19 @@ import (
 	"time"
 )
 
+type Foo int
+type Args struct{ Num1, Num2 int }
+
+func (f *Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
+	var foo Foo
+	if err := rpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal("network error:", err)
@@ -33,8 +44,8 @@ func main() {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("geerpc req %d", i)
-			var reply string
+			args := Args{Num1: i, Num2: i + 1}
+			var reply int
 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
--- rpc/server.go
+++ rpc/server.go
@@ -3,13 +3,15 @@ package rpc
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
+	"go/ast"
 	"io"
 	"log"
 	"net"
 	"reflect"
 	"rpc/codec"
+	"strings"
 	"sync"
+	"sync/atomic"
 )
 
 const MagicNumber = 0x3bef5c
@@ -23,13 +25,139 @@ var invalidRequest = struct{}{}
 type request struct {
 	h           *codec.Header
 	argv, reply reflect.Value
+	mtype       *methodType
+	svc         *service
 }
 
 type Option struct {
 	MagicNumber int
 	CodecType   codec.Type
 }
-type Server struct{}
+
+type methodType struct {
+	method    reflect.Method
+	ArgType   reflect.Type
+	ReplyType reflect.Type
+	numCalls  uint64
+}
+
+func (m *methodType) NumCalls() uint64 {
+	return atomic.LoadUint64(&m.numCalls)
+}
+func (m *methodType) newArgv() reflect.Value {
+	var argv reflect.Value
+	if m.ArgType.Kind() == reflect.Ptr {
+		argv = reflect.New(m.ArgType.Elem())
+	} else {
+		argv = reflect.New(m.ArgType).Elem()
+	}
+	return argv
+}
+
+func (m *methodType) newReply() reflect.Value {
+	replyV := reflect.New(m.ReplyType.Elem())
+
+	switch m.ReplyType.Elem().Kind() {
+	case reflect.Map:
+		replyV.Elem().Set(reflect.MakeMap(m.ReplyType.Elem()))
+	case reflect.Slice:
+		replyV.Elem().Set(reflect.MakeSlice(m.ReplyType.Elem(), 0, 0))
+
+	}
+	return replyV
+}
+
+type service struct {
+	name   string
+	typ    reflect.Type
+	rcvr   reflect.Value
+	method map[string]*methodType
+}
+
+func newService(rcvr any) *service {
+	s := new(service)
+	s.rcvr = reflect.ValueOf(rcvr)
+	s.name = reflect.Indirect(s.rcvr).Type().Name()
+
+	s.typ = reflect.TypeOf(rcvr)
+	if !ast.IsExported(s.name) {
+		log.Fatalf("rpc server:%s is not a valid service name", s.name)
+	}
+	s.registerMethods()
+	return s
+}
+
+func (s *service) registerMethods() {
+	s.method = make(map[string]*methodType)
+	for i := 0; i < s.typ.NumMethod(); i++ {
+		method := s.typ.Method(i)
+		mType := method.Type
+		if mType.NumIn() != 3 || mType.NumOut() != 1 {
+			continue
+		}
+		if mType.Out(0) != reflect.TypeOf((*error)(nil)).Elem() {
+			continue
+		}
+		argType, replyType := mType.In(1), mType.In(2)
+		if !isExportedOrBuiltinType(argType) || !isExportedOrBuiltinType(replyType) {
+			continue
+		}
+
+		s.method[method.Name] = &methodType{
+			method:    method,
+			ArgType:   argType,
+			ReplyType: replyType,
+		}
+		log.Printf("rpc server:register %s.%s\n", s.name, method.Name)
+	}
+}
+func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
+	atomic.AddUint64(&m.numCalls, 1)
+	f := m.method.Func
+	returnValues := f.Call([]reflect.Value{s.rcvr, argv, replyv})
+	if errInter := returnValues[0].Interface(); errInter != nil {
+		return errInter.(error)
+	}
+	return nil
+}
+
+func isExportedOrBuiltinType(t reflect.Type) bool {
+	return ast.IsExported(t.Name()) || t.PkgPath() == ""
+}
+
+type Server struct {
+	serviceMap sync.Map
+}
+
+func (server *Server) Register(rcvr any) error {
+	s := newService(rcvr)
+	if _, dup := server.serviceMap.LoadOrStore(s.name, s); dup {
+		return errors.New("rpc: service already defined:" + s.name)
+	}
+	return nil
+}
+
+func (server *Server) findService(serviceMethod string) (svc *service, mtype *methodType, err error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		err = errors.New("rpc server:service/method request ill-formed:" + serviceMethod)
+		return
+	}
+
+	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot+1:]
+	svci, ok := server.serviceMap.Load(serviceName)
+	if !ok {
+		err = errors.New("rpc server:can't find service:" + serviceName)
+		return
+	}
+	svc = svci.(*service)
+	mtype, ok = svc.method[methodName]
+	if !ok {
+		err = errors.New("rpc server:can't find method:" + methodName)
+		return
+	}
+	return svc, mtype, nil
+}
 
 func NewServer() *Server {
 	return &Server{}
@@ -89,9 +217,21 @@ func (s *Server) readRequest(cc codec.Codec) (req *request, err error) {
 		return nil, err
 	}
 	req = &request{h: &h}
-	req.argv = reflect.New(reflect.TypeOf(""))
-	if err = cc.ReadBody(req.argv.Interface()); err != nil {
+	req.svc, req.mtype, err = s.findService(h.ServiceMethod)
+	if err != nil {
+		return req, err
+	}
+
+	req.argv = req.mtype.newArgv()
+	req.reply = req.mtype.newReply()
+
+	argvi := req.argv.Interface()
+	if req.argv.Type().Kind() != reflect.Ptr {
+		argvi = req.argv.Addr().Interface()
+	}
+	if err = cc.ReadBody(argvi); err != nil {
 		log.Println("rpc server: read body error:", err)
+		return req, err
 	}
 	return req, err
 
@@ -106,8 +246,11 @@ func (s *Server) sendResponse(cc codec.Codec, header *codec.Header, body any, mu
 }
 func (s *Server) handleRequest(cc codec.Codec, req *request, mutex *sync.Mutex, wg *sync.WaitGroup) {
 	defer wg.Done()
-	log.Println(req.h, req.argv.Elem())
-	req.reply = reflect.ValueOf(fmt.Sprintf("rpc resp %d", req.h.Seq))
+	err := req.svc.call(req.mtype, req.argv, req.reply)
+	if err != nil {
+		req.h.Error = err.Error()
+		s.sendResponse(cc, req.h, invalidRequest, mutex)
+	}
 	s.sendResponse(cc, req.h, req.reply.Interface(), mutex)
 
 }
@@ -126,3 +269,5 @@ func (s *Server) Accept(listener net.Listener) {
 func Accept(listener net.Listener) {
 	DefaultServer.Accept(listener)
 }
+
+func Register(rcvr any) error { return DefaultServer.Register(rcvr) }
